db401fe46d9ec7d298603e9a4bf6f9b0e16500ea
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-07-21 00:47:54 +0100

    authentication

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import { useEffect } from 'react';
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { usePuterStore } from './lib/puter';
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,15 +26,25 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { init } = usePuterStore();
+
+  useEffect(() => {
+    init();
+  }, [init]);
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
         <Meta />
         <Links />
       </head>
       <body>
+        <script src='https://js.puter.com/v2/'></script>
+
         {children}
         <ScrollRestoration />
         <Scripts />

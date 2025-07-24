8990d56a855a951a2a7345f47820ffa7776024fb
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-07-24 22:50:50 +0100

    Update resume.tsx
    new

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,6 @@
-import {Link, useNavigate, useParams} from "react-router";
-import {useEffect, useState} from "react";
-import {usePuterStore} from "~/lib/puter";
+import { Link, useNavigate, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
 import Summary from "~/components/Summary";
 import ATS from "~/components/ATS";
 import Details from "~/components/Details";
@@ -8,43 +8,34 @@ import Details from "~/components/Details";
 export const meta = () => ([
     { title: 'Resumind | Review ' },
     { name: 'description', content: 'Detailed overview of your resume' },
-])
+]);
 
 const Resume = () => {
     const { auth, isLoading, fs, kv } = usePuterStore();
     const { id } = useParams();
-    const [imageUrl, setImageUrl] = useState('');
     const [resumeUrl, setResumeUrl] = useState('');
     const [feedback, setFeedback] = useState<Feedback | null>(null);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if(!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume/${id}`);
-    }, [isLoading])
+        if (!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume/${id}`);
+    }, [isLoading]);
 
     useEffect(() => {
         const loadResume = async () => {
             const resume = await kv.get(`resume:${id}`);
-
-            if(!resume) return;
+            if (!resume) return;
 
             const data = JSON.parse(resume);
-
             const resumeBlob = await fs.read(data.resumePath);
-            if(!resumeBlob) return;
+            if (!resumeBlob) return;
 
             const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' });
             const resumeUrl = URL.createObjectURL(pdfBlob);
             setResumeUrl(resumeUrl);
 
-            const imageBlob = await fs.read(data.imagePath);
-            if(!imageBlob) return;
-            const imageUrl = URL.createObjectURL(imageBlob);
-            setImageUrl(imageUrl);
-
             setFeedback(data.feedback);
-            console.log({resumeUrl, imageUrl, feedback: data.feedback });
-        }
+        };
 
         loadResume();
     }, [id]);
@@ -59,12 +50,12 @@ const Resume = () => {
             </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
                 <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
-                    {imageUrl && resumeUrl && (
+                    {resumeUrl && (
                         <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
                             <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
-                                <img
-                                    src={imageUrl}
-                                    className="w-full h-full object-contain rounded-2xl"
+                                <iframe
+                                    src={resumeUrl}
+                                    className="w-full h-full rounded-2xl"
                                     title="resume"
                                 />
                             </a>
@@ -85,6 +76,7 @@ const Resume = () => {
                 </section>
             </div>
         </main>
-    )
-}
-export default Resume
+    );
+};
+
+export default Resume;
